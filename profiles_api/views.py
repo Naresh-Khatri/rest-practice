c5857148085175b3909978d46afcb5679b11bc9a
naresh-khatri <naresh.khatri2345@gmail.com>
2020-07-08 21:57:35 +0530

    Added viewset to profiles_api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import viewsets
 from rest_framework import status
 
 from profiles_api import serializers
@@ -38,3 +39,41 @@ class HelloApiView(APIView):
 
     def delete(self,request, pk=None):
         return Response({'method': "DELETE"})
+
+
+class HelloViewSet(viewsets.ViewSet):
+
+    serializer_class = serializers.HelloSerializer
+    def list(self, request):
+        a_viewset=[
+        'usefasfjasdlf',
+        'adsfohefhbbhfhfhdsfsd',
+        'jirhjjggurtj sfjosies fsd fosaijfo asdf as dfas df Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        ]
+        return Response({'message':'hello', 'a_viewset': a_viewset})
+
+
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'hello{name}!'
+            return Response({'message': message})
+        else:
+            return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def retriev(self, request, pk=None):
+        return Response({'HTTP method': 'PUT'})
+
+    def update(self, request, pk=None):
+        return Response({'HTTP method': 'update'})
+
+    def partial_update(self, request, pk=None):
+        return Response({'HTTP method': 'partial_update'})
+
+    def destroy(self, request, pk=None):
+        return Response({'HTTP method': 'delete'})
