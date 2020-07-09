acd93ac2b42077452176a9f01955c1010abbfa14
naresh-khatri <naresh.khatri2345@gmail.com>
2020-07-09 22:54:20 +0530

    configured for deployment

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'tgc_p32f40!$2)1(g@6md)9^wc7lfbif=_@9*a0d^qy0zwii9z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environment.get('DEBUG',1))
 
 ALLOWED_HOSTS = []
 
@@ -123,3 +123,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL ="profiles_api.UserProfile"
+
+STATIC_ROOT = 'static/'
